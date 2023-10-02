4f3e13e748f8b8e7fd2bba3524eeceb657cff993
Onejohi <tonnie254@live.com>
2023-10-02 09:33:48 +0300

    - initialize application

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+  <ul class="nav justify-content-end">
+    <li class="nav-item">
+      <RouterLink to="/" class="nav-link active">Patient List</RouterLink>
+    </li>
+    <li class="nav-item">
+      <RouterLink to="/about" class="nav-link active">New Visit</RouterLink>
+    </li>
+  </ul>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
